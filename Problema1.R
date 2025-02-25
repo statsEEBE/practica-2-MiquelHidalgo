b79ee6fb924a572bc65ddbbfbd59e03a9dd96e49
MiquelHidalgo <miquel.hidalgo@estudiantat.upc.edu>
2025-02-25 10:01:19 +0100

    problemas 1,2

--- Problema1.R
+++ Problema1.R
@@ -1,1 +1,12 @@
 #Codigo para problema 1
+
+Rsq <- 0.1923#R^2
+cor <- sqrt(Rsq)#R
+m <- 0.06576
+b <- 11.79755
+
+m*647.1+b
+
+
+#cargar datos de ficehro de texto
+data <- read.delime("air.txt")
--- Problema2.R
+++ Problema2.R
@@ -1,1 +1,43 @@
 #Codigo para problema 2
+mis_dades <- iris
+mis_dades
+mean(mis_dades$Sepal.Length)
+sd(mis_dades$Sepal.Length)#desviacion
+dim(mis_dades)
+names(mis_dades)
+hist(mis_dades$Sepal.Length)
+
+x <- mis_dades$Petal.Length
+x
+y <- mis_dades$Sepal.Length
+y
+
+plot(x,y)
+
+# m=(sum(x-mean(x))*(y-mean(y)))/(sum(x-mean(x))^2) -> pendent recta
+m <- sum((x-mean(x))*(y-mean(y)))/sum((x-mean(x))^2)
+m
+#b = intersecció amb l'eix y
+b <- mean(y)-m*mean(x)
+b
+#pregunta 3
+m*1.5+b
+
+lm(y~x)
+mod <- lm(y~x)
+summary(mod)
+
+xpred <- data.frame(x=1.5)
+xpred <- data.frame(x=1:7)
+xpred <- data.frame(x=x)
+xpred
+ypred <- predict(mod,xpred)
+ypred
+
+plot(x,y)
+lines(x,ypred) #regressio
+#R^2 = (sum(ypred-mean(y))^2)/(sum(y-mean(y))^2)
+Rsq <- sum((ypred-mean(y)^2))/sum((y-mean(y)^2))#calculo de R^2
+Rsq <- sum((ypred-mean(y)^2))/sum((y-mean(y)^2))
+Rsq
+#coeficiente de correlacion de pearson = sqrt(R^2)
